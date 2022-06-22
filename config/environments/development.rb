22bd45435556604f9b0001b43c42396f714c2d47
nvdung2411 <nvdung.dev@gmail.com>
2022-06-22 09:37:49 +0700

    User authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -32,7 +34,27 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = {host: ENV["host"]}
+ #  config.action_mailer.smtp_settings = {
+	#   :user_name => 'dungtd932k@gmail.com',
+	#   :password => 'ktliugxpitzojnym',
+	#   :address => 'smtp.mailtrap.io',
+	#   :domain => 'smtp.mailtrap.io',
+	#   :port => '2525',
+	#   :authentication => :cram_md5
+	# }
+	config.action_mailer.default_url_options = {host: "localhost:3000"}
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["EMAIL"],
+    password: ENV["PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+}
 
   config.action_mailer.perform_caching = false
 
